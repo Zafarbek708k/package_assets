af32e8667442eff4846c481821aacb93730e7c63
z.karimov <z.karimov@uic.group>
2025-06-05 16:20:10 +0500

    add webp images

--- lib/core/config/app_webp.dart
+++ lib/core/config/app_webp.dart
@@ -4,4 +4,6 @@ sealed class AppWebp{
 
   static const insuranceBlur = 'assets/webp/Insurance_blur_card.webp';
   static const loginVector = 'assets/webp/login_vector.webp';
+  static const poeBlur = 'assets/webp/poe_blur.webp';
+  static const tintingBlur = 'assets/webp/tinting_blur.webp';
 }
