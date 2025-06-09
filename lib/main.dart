e1f916dfb75dba284ca22c73076484c1eec5ab41
z.karimov <z.karimov@uic.group>
2025-06-09 13:18:33 +0500

    icons next + radar icons

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'dart:io';
+import 'package:flutter/material.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  final dir = Directory('assets/icons');
+  final files = dir.listSync().where((file) => file is File && file.path.endsWith('.svg')).toList();
+
+  final buffer = StringBuffer();
+  buffer.writeln('final iconPaths = [');
+
+  for (int i = 0; i < files.length; i++) {
+    final path = files[i].path.replaceAll('\\', '/');
+    buffer.writeln("  '$path',");
+  }
+
+  buffer.writeln('];');
+
+  File('lib/generated_icons.dart').writeAsStringSync(buffer.toString());
+
+  print('✅ lib/generated_icons.dart fayli yaratildi!');
+
+  runApp(MaterialApp(home: Scaffold(appBar: AppBar(title: Text("Title")))));
+}
