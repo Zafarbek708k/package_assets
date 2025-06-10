59f89704f398a8a3a766d03ab4f16a5a06015eee
z.karimov <z.karimov@uic.group>
2025-06-10 08:50:37 +0500

    webp file fixed

--- lib/core/config/package_webp.dart
+++ lib/core/config/package_webp.dart
@@ -5,4 +5,6 @@ class PackageWebp {
   final loginVector = 'assets/webp/login_vector.webp';
   final poeBlur = 'assets/webp/poe_blur.webp';
   final tintingBlur = 'assets/webp/tinting_blur.webp';
+  final technicalInspectionBlur = 'assets/webp/technical_inspection_blur.webp';
+  final noInternet = 'assets/webp/no_internet.webp';
 }
